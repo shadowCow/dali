ad725480a36a0c80c5dadbe049b27c9301c99dcc
Dan Wadeson <dan.wadeson@tryonsolutions.com>
2018-08-05 15:42:18 -0400

    ability to draw eye pair

---
+++ src/model/actions/compositeActions/eye.js
@@ -0,0 +1,52 @@
+import actionCreators from '../actionCreators'
+const {drawCircle, drawEllipse} = actionCreators
+
+function drawEye(
+  id,
+  centerX,
+  centerY,
+  irisRadius,
+  irisColor
+) {
+  return [
+    drawEyeOutline(id, centerX, centerY, irisRadius),
+    drawIris(id, centerX, centerY, irisRadius, irisColor),
+    drawPupil(id, centerX, centerY, irisRadius/2),
+  ]
+}
+
+function drawIris(eyeId, centerX, centerY, radius, color) {
+  return drawCircle(eyeId + "-iris", centerX, centerY, radius, color)
+}
+
+function drawPupil(eyeId, centerX, centerY, radius) {
+  return drawCircle(eyeId + "-pupil", centerX, centerY, radius, "black")
+}
+
+function drawEyeOutline(eyeId, centerX, centerY, irisRadius) {
+  return drawEllipse(
+    eyeId + "-eyeOutline",
+    centerX,
+    centerY,
+    irisRadius*2,
+    irisRadius*1.25,
+    "white",
+    "black",
+    3
+  )
+}
+
+function drawEyePair(id, centerX, centerY, eyeSpacing, irisRadius, irisColor) {
+  const centerXEye1 = centerX - eyeSpacing/2 - irisRadius*2
+  const centerXEye2 = centerX + eyeSpacing/2 + irisRadius*2
+
+  return [
+    ...drawEye(id + "-eyeOne", centerXEye1, centerY, irisRadius, irisColor),
+    ...drawEye(id + "-eyeTwo", centerXEye2, centerY, irisRadius, irisColor)
+  ]
+}
+
+export default {
+  drawEye,
+  drawEyePair
+}
