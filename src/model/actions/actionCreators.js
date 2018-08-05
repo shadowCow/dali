6b51fce90da82220374f875b06bbb1effeaeb0f3
Dan Wadeson <dan.wadeson@tryonsolutions.com>
2018-08-05 15:09:18 -0400

    ability to draw waves

--- src/model/actions/actionCreators.js
+++ src/model/actions/actionCreators.js
@@ -164,6 +164,27 @@ function drawPath(
   return action
 }
 
+function drawWave(
+  id,
+  startX,
+  startY,
+  waveLength,
+  amplitude,
+  numCycles,
+  fill,
+  stroke,
+  strokeWidth
+) {
+  let d = `M ${startX},${startY} ` +
+           `Q ${startX + waveLength/2},${startY - amplitude} ${startX + waveLength},${startY} ` +
+           "t"
+
+  for (let i = 0; i < numCycles; i++) {
+    d += ` ${waveLength},0`
+  }
+  return drawPath(id, d, fill, stroke, strokeWidth)
+}
+
 export default {
   drawCircle,
   drawEllipse,
@@ -172,4 +193,5 @@ export default {
   drawPolyline,
   drawPolygon,
   drawPath,
+  drawWave,
 }
--- src/stateInitializer.js
+++ src/stateInitializer.js
@@ -4,12 +4,13 @@ import PaintingConstants from './model/PaintingConstants'
 import actionCreators from './model/actions/actionCreators'
 
 const {CIRCLE} = PaintingConstants
-const {drawCircle, drawPath} = actionCreators
+const {drawCircle, drawPath, drawWave} = actionCreators
 
 function getInitialActions() {
   return [
     drawCircle('1',100,100,50,"red","black",3),
-    drawPath("2","M 200,100 A 30 60 20 1 1 250 250")
+    drawPath("2","M 200,100 A 30 60 20 1 1 250 250","green"),
+    drawWave("3",50, 50, 50, 40, 20,"none","blue",3)
   ]
 }
 
