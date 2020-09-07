c0101386459982195471283fc0d95bba8a285754
Pop Wadeson <wadeson.doug@gmail.com>
2020-09-07 10:56:24 -0400

    Color change

--- src/boot/pop/boot.pop.ts
+++ src/boot/pop/boot.pop.ts
@@ -24,7 +24,7 @@ function exampleScene(
             Transform.create({
                 translate: {x: 100, y: 100},
             }),
-            fill(Colors.Red()),
+            fill(Colors.Blue()),
         ),
     ];
 
